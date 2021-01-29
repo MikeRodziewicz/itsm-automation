dda027dde370d2affd265e7356006a57b337c495
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2021-01-29 21:46:20 +0100

    joining stp and hs data

--- data_manipulation/daily_report_hs.py
+++ data_manipulation/daily_report_hs.py
@@ -14,3 +14,11 @@ def _read_hs_daily_data():
     hs_daily['Created'] = pd.to_datetime(hs_daily['Created'].str[:11])
     return hs_daily
 
+def _combine_hs_and_stp():
+    """Join HS data with STP responsible"""
+    hs_data = _read_hs_daily_data()
+    stp_data = _make_stp_map_df()
+    df_for_daily_rep = pd.DataFrame(hs_data[['Ticket', 'Created', 'Score', 'Comment', 'Factor', 'Assignment Group']])
+    df_for_daily_rep = pd.merge(df_for_daily_rep, stp_data, on='Assignment Group', how='left')
+    return df_for_daily_rep
+
