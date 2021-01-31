5340577b588c2ae7387ffa1a323e3fc7443c7416
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2021-01-31 12:29:32 +0100

    added hs filtering

--- data_manipulation/daily_report_hs.py
+++ data_manipulation/daily_report_hs.py
@@ -6,31 +6,38 @@ def _make_stp_map_df():
     """Create dataframe with stp mapping"""
     stp_df = pd.read_csv('~/Documents/data_for_hs/STP_MAP.csv')
     stp_df = pd.DataFrame(stp_df[['Assignment Group', 'STP']])
+    
     return stp_df
 
 def _read_hs_daily_data():
     """Read and return df for daily HS extract"""
     hs_daily = pd.read_csv('~/Documents/data_for_hs/hs_data.csv',header=4, skip_blank_lines=False )
     hs_daily['Created'] = pd.to_datetime(hs_daily['Created'].str[:11])
+
     return hs_daily
 
 def _combine_hs_and_stp():
     """Join HS data with STP responsible"""
     hs_data = _read_hs_daily_data()
     stp_data = _make_stp_map_df()
+
     df_for_daily_rep = pd.DataFrame(hs_data[
         ['Ticket', 'Created', 'Score', 'Comment', 'Factor', 'Assignment Group']
     ])
     df_for_daily_rep = pd.merge(df_for_daily_rep, stp_data, on='Assignment Group', how='left')
+
     return df_for_daily_rep
 
 def create_daily_HS_report():
     """Creates report for daily HS review"""
     daily_hs_rep = _combine_hs_and_stp()
+
     count_in_weekends = (daily_hs_rep['Created'] >= str(DateStamps().count_in_weekend()))
     daily_hs_rep = daily_hs_rep.loc[count_in_weekends]
+
     count_in_today = (daily_hs_rep['Created'] < str(DateStamps().get_today()))
     daily_hs_rep = daily_hs_rep.loc[count_in_today]
+
     return daily_hs_rep
 
 def create_dsr_report():
@@ -41,6 +48,7 @@ def create_dsr_report():
     if DateStamps().get_today() == DateStamps().get_start_month():
         count_in_weekends = (base_dsr_df['Created'] >= str(DateStamps().count_in_weekend()))
         dsr_slide_one = base_dsr_df.loc[count_in_weekends]
+
     count_in_today = (base_dsr_df['Created'] < str(DateStamps().get_today()))
     dsr_slide_one = base_dsr_df.loc[count_in_today]
     dsr_slide_one = pd.DataFrame(dsr_slide_one[['Score', 'STP']])
--- data_manipulation/weekly_hs_breakdown.py
+++ data_manipulation/weekly_hs_breakdown.py
@@ -2,10 +2,35 @@ import pandas as pd
 from files.folder_creation import DateStamps
 from daily_report_hs import _combine_hs_and_stp
 
-dframe1 = _combine_hs_and_stp()
-dframe_group = pd.DataFrame(dframe1)
+
+def _create_weekly_hs_base():
+    """Create base dframe for weekly breakdown"""
+    hs_weekly_base = _combine_hs_and_stp()
+    return hs_weekly_base
+
+def _filter_negative_hs(base_dframe):
+    """Retrieve scores between 1 and 3"""
+    hs_negative_df = base_dframe[base_dframe["Score"].isin([1,2,3])]
+    return hs_negative_df
+
+def _filter_neutral_hs(base_dframe):
+    """Retrieve scores between 4 and 8"""
+    hs_neutral_df = base_dframe[base_dframe["Score"].isin([4,5,6,7,8])]
+    return hs_neutral_df
+
+def _filter_positive_hs(base_dframe):
+    """Retrieve scores between 9 and 10"""
+    hs_positive_df = base_dframe[base_dframe["Score"].isin([9,10])]
+    return hs_positive_df
+
+def create_hs_breakdown(base_dfame):
+    """Returns three filtered dframes"""
+    hs_negative = _filter_negative_hs(base_dfame)
+    hs_neutral = _filter_neutral_hs(base_dfame)
+    hs_positive = _filter_positive_hs(base_dfame)
+
+    return hs_negative, hs_neutral, hs_positive
 
 
 
-# print(dframe_group.groupby('Score'))
-# print(dframe_group.describe())
+
