956a84e3818899e8daede34bb4334139d8866db3
MikeRodziewicz <michal.rodziewicz1@gmail.com>
2021-01-31 21:23:56 +0100

    added split factors into a list

--- data_manipulation/weekly_hs_breakdown.py
+++ data_manipulation/weekly_hs_breakdown.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from files.folder_creation import DateStamps
 from daily_report_hs import _combine_hs_and_stp
+from collections import Counter
 
 
 def _create_weekly_hs_base():
@@ -23,14 +24,39 @@ def _filter_positive_hs(base_dframe):
     hs_positive_df = base_dframe[base_dframe["Score"].isin([9,10])]
     return hs_positive_df
 
-def create_hs_breakdown(base_dfame):
+def create_hs_breakdown(base_dframe):
     """Returns three filtered dframes"""
-    hs_negative = _filter_negative_hs(base_dfame)
-    hs_neutral = _filter_neutral_hs(base_dfame)
-    hs_positive = _filter_positive_hs(base_dfame)
+    hs_negative = _filter_negative_hs(base_dframe)
+    hs_neutral = _filter_neutral_hs(base_dframe)
+    hs_positive = _filter_positive_hs(base_dframe)
 
     return hs_negative, hs_neutral, hs_positive
 
+#TODO Try to separate comments count 
+
+def _string_splitter(category_dframe):
+    """Read dframe for category to get a list of factors"""
+    cat_dframe = category_dframe[category_dframe['Factor'].notnull()]
+    list_of_factors = cat_dframe['Factor'].tolist()
+    split_list_factors = []
+    for item in list_of_factors:
+        split_list_factors.extend(item.split(', '))            
+    return split_list_factors
+
+def _factor_counter(splitted_list):
+    """Calculates occurences of factors"""
+    calculated_list = Counter(splitted_list).most_common()
+    return calculated_list
+
+def create_factor_breakdown(counted_factor_list):
+    """Returns three calculated factors groups"""
+    for counted_list in counted_factor_list:
+        splitted = _string_splitter(counted_list)
+        counted = _factor_counter(splitted)
+    return counted
 
 
 
+
+#TODO analyze top offenders for each category
+#TODO calculate avarage for the week 
